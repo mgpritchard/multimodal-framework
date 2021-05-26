9a4f1bd9bec73b3f2e80920f173840b784b66904
Michael <mgpritchard123@yahoo.co.uk>
2021-05-26 20:21:41 +0100

    Adding live EMG functionality
    Adding various functions to achieve real-time EMG classification via PyoConnect, including the testing of fusion concepts through trivially dividing and re-fusing the EMG

--- topLevel.py
+++ topLevel.py
@@ -19,7 +19,7 @@ from queue import Queue
 import logging
 from threading import Thread
 import time
-import handleEMG
+from handleEMG import *
 from handleML import *
 from handleOffline import *
 from handleFusion import *
@@ -33,6 +33,13 @@ def liveclassify():
     threadEEG.start()
     threadML.start()
     return
+
+def onlineclassify():
+    path=os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    model1=load_model('mode 1',path)
+    model2=load_model('mode 2',path)
+    time.sleep(1)
+    pyoc_run_split(model1,model2)
 
 def offlineclassify(toggle_report):
     #model_name = 'EMG-GNB-SYNTH-CALIB.sav'  #load GPT2-augmented model
@@ -50,9 +57,9 @@ def offlineclassify(toggle_report):
     quickplot(mode2,'mode 2 after')
     offset=0
     period=1
-    pred_array1=[]
-    pred_array2=[]
-    pred_arrayfusion=[]
+    #pred_array1=[] #this approach etc used previously when separate arrs
+    colnames=['Mode 1','Mode 2','Fusion','Weighting 1','Weighting 2']
+    preds={col: [] for col in colnames}
     while (offset <= (mode1[-1,0]-mode1[0,0])-period):
         #print('start: %f' % time.time())
         tstart=time.time()
@@ -62,19 +69,26 @@ def offlineclassify(toggle_report):
         
         gesture1=pred_gesture(pred1,0)
         gesture2=pred_gesture(pred2,0)
-        pred_array1.append(str(gesture1[0]))
-        pred_array2.append(str(gesture2[0]))
+        #pred_array1.append(str(gesture1[0])) #this approach previously used
+        preds["Mode 1"].append(str(gesture1[0]))
+        preds["Mode 2"].append(str(gesture2[0]))
         
         mean_distro=fuse_mean(distro1,distro2)
         gesture_mean=pred_from_distro(classlabels,mean_distro)
-        pred_arrayfusion.append(str(gesture_mean))
+        preds["Fusion"].append(str(gesture_mean))
         
-        #conf_distro=fuse_conf(distro1,distro2)
-        #gesture_conf=pred_from_distro(classlabels,conf_distro)
-        #pred_arrayfusion.append(str(gesture_conf))
+        w1_distro=fuse_linweight(distro1,distro2,75,25) #bundle these lines?
+        gesture_w1=pred_from_distro(classlabels,w1_distro)
+        preds["Weighting 1"].append(str(gesture_w1))
         
-        print(time.time(),'\n','Mode 1: ',gesture1,'\n','Mode 2: ',gesture2,'\n','Fusion: ',gesture_mean,'\n-------------')
-       
+        w2_distro=fuse_linweight(distro1,distro2,25,75)
+        gesture_w2=pred_from_distro(classlabels,w2_distro)
+        preds["Weighting 2"].append(str(gesture_w2))
+        
+        print(time.time(),'\n','Mode 1: ',gesture1,'\n','Mode 2: ',gesture2,'\n','Fusion: ',gesture_mean)#,'\n-------------')
+        print('Weighted to 1: ',gesture_w1,'\nWeighted to 2: ',gesture_w2)
+        print('-------------')
+        
         offset+=0.5*period
         #quickplot(current_slice,'mode 1 slice n')
         tend=time.time()
@@ -87,10 +101,13 @@ def offlineclassify(toggle_report):
     
     if toggle_report:
         state=state.capitalize()
-        scores = eval_fusion(pred_array1,pred_array2,pred_arrayfusion,state)
+        #scores=eval_multi([pred_array1,pred_array2,pred_arrayfusion,pred_array_w1,pred_array_w2],state) #make these all one array!!
+        scores=eval_multi(list(preds.values()),state)
         print(scores)
-        acc = pd.DataFrame([scores],columns=['Mode 1','Mode 2','Fusion'])
-        result = pd.DataFrame(list(zip(pred_array1,pred_array2,pred_arrayfusion)),columns=['Mode 1','Mode 2','Fusion'])
+        acc = pd.DataFrame([scores],columns=colnames)
+        #result = pd.DataFrame(list(zip(pred_array1,pred_array2,pred_arrayfusion,pred_array_w1,pred_array_w2)),columns=['Mode 1','Mode 2','Fusion','Mostly 1','Mostly 2'])
+        result = pd.DataFrame.from_dict(preds)
+        result.columns=colnames
         return result, acc
     
     return
@@ -106,7 +123,7 @@ if __name__ == '__main__':
     #ALLOW parameters passed from commandline, but if CLI inputs are none
     #THEN ask for user input instead
     
-    onlinemode=0#int(input('Classifying live? 1:Y 0:N '))
+    onlinemode=1#int(input('Classifying live? 1:Y 0:N '))
     print(onlinemode)
     
     #IDEAL could be a basic gui unless you CLI override to use the text input
@@ -124,4 +141,12 @@ if __name__ == '__main__':
         #train_offline()
     
     else:
-        liveclassify()
+        #onlineclassify()
+        #liveclassify()
+        path=os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        model1=load_model('mode 1',path)
+        results=pyoc_slicetester(model1)
+        
+        #class labels might be wrong?
+
+    #https://stackoverflow.com/questions/47096507/reducing-the-number-of-arguments-in-function-in-python

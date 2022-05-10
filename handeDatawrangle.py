33eb03b28d258b666098f8280b06d9cb02f5655b
Michael <mgpritchard123@yahoo.co.uk>
2022-05-10 21:12:38 +0100

    Adding raw data wrangling functionality
    Initial commit, untested

---
+++ handeDatawrangle.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 21:09:41 2022
+
+@author: pritcham
+
+module to contain functionality related to wrangling & processing of data
+"""
+import os, sys
+import numpy as np
+#import pandas as pd
+#import matplotlib.pyplot as plt
+import csv
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename, askopenfilenames, askdirectory
+from queue import Queue
+import logging
+from threading import Thread
+import time
+from handleEMG import *
+from handleML import *
+from handleOffline import *
+from handleFusion import *
+import pickle
+import plot_dists as proto_plot
+
+def get_dir(datatype='',stage=''):
+    homepath=os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    Tk().withdraw()
+    if stage=='raw':
+        title='directory of raw '+datatype+' time series data'
+    elif stage== 'crop':
+        title='directory for cropped raw '+datatype+' time series data'
+    elif stage=='proc':
+        title='directory for processed '+datatype+' time series data'
+    set_dir=askdirectory(title=title,initialdir=homepath)
+    return set_dir
+
+class Rawfile:
+    def __init__(self,filepath,ppt,label,count):
+        self.filepath=filepath
+        self.ppt=ppt
+        self.label=label
+        self.count=count
+    
+    def __repr__(self):
+        return(f'{self.__class__.__name__}('
+               f'{self.ppt!r}, {self.label!r}, {self.count!r})')
+
+def list_raw_files(raw_dir):
+    raw_list = None
+    for x in os.listdir(raw_dir):
+        if not x.lower().endswith('.csv'): # Ignore non-CSV files
+            continue    
+        # For safety we'll ignore files containing the substring "test". 
+        # [Test files should not be in the dataset directory in the first place]
+        if 'test' in x.lower():
+            continue
+        try:
+            name, state, count = x[:-4].split('-')
+        except:
+            print ('Wrong file name', x)
+            sys.exit(-1)   
+        print ('Using file', x)
+        full_file_path = raw_dir + '/' + x
+        rawfile = Rawfile(full_file_path,name,state,count)
+        raw_list.append(rawfile)
+    return raw_list
+
+def sync_raw_files(raw_emg_list,raw_eeg_list,crop_emg_dir,crop_eeg_dir):
+    for emgfile in raw_emg_list:
+        print('working with '+repr(emgfile))
+        raw_emg = matrix_from_csv_file(emgfile.filepath)
+        eegfile= next((eeg for eeg in raw_eeg_list
+                 if (eeg.ppt==emgfile.ppt and eeg.label==emgfile.label
+                  and eeg.count == emgfile.count)),None)
+        if eegfile is None:
+            print('no matching eeg file... :(')
+            print('skipping')
+            continue
+        print('found eeg: '+repr(eegfile))
+        raw_eeg = matrix_from_csv_file(eegfile.filepath)
+        if 1:
+            crop_emg,crop_eeg=approve_sync(raw_emg, raw_eeg)
+        else:
+            crop_emg,crop_eeg=sync_crop(raw_emg, raw_eeg)
+        np.savetxt(build_path(crop_emg_dir,emgfile),crop_emg,delimiter=',')
+        np.savetxt(build_path(crop_emg_dir,eegfile),crop_eeg,delimiter=',')
+        
+def build_path(dirpath,file):
+    filepath=dirpath+'/'+file.ppt+'-'+file.label+'-'+file.count+'.csv'
+    return filepath
+
+def approve_sync(mode1,mode2):
+    quickplot(mode1,'mode 1 before')
+    quickplot(mode2,'mode 2 before')
+    mode1crop,mode2crop=sync_crop(mode1,mode2)
+    quickplot(mode1,'mode 1 after')
+    quickplot(mode2,'mode 2 after')
+    approval=input('approve crop? [1/0] ')
+    if approval:
+        return mode1crop,mode2crop
+    else:
+        print('returning uncropped')
+        return mode1,mode2
+        
+def matrix_from_csv_file(file_path):
+    csv_data = np.genfromtxt(file_path, delimiter = ',')
+    full_matrix = csv_data[1:]
+    #headers = csv_data[0] # Commented since not used or returned [fcampelo]
+    return full_matrix
+    
+def whole_pipeline():
+    raw_emg_dir=get_dir('emg','raw')
+    crop_emg_dir=get_dir('emg','crop')
+    proc_emg_dir=get_dir('emg','proc')
+    raw_eeg_dir=get_dir('eeg','raw')
+    crop_eeg_dir=get_dir('eeg','crop')
+    proc_eeg_dir=get_dir('eeg','proc')
+    raw_emg_list=list_raw_files(raw_emg_dir)
+    raw_eeg_list=list_raw_files(raw_eeg_dir)
+    sync_raw_files(raw_emg_list,raw_eeg_list,crop_emg_dir,crop_eeg_dir)

2f6e855556095cf9f5d3f4f08758285730777f96
Michael <mgpritchard123@yahoo.co.uk>
2022-05-26 16:32:05 +0100

    General data proc updates + rename
    Renamed to correct typo, added equality check for data file object class, basic skeleton for EEG processing

--- handleDatawrangle.py
+++ handleDatawrangle.py
@@ -14,16 +14,16 @@ import numpy as np
 import csv
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename, askopenfilenames, askdirectory
-from queue import Queue
-import logging
-from threading import Thread
-import time
+#from queue import Queue
+#import logging
+#from threading import Thread
+#import time
 from handleEMG import *
 from handleML import *
-from handleOffline import *
+from handleOffline import sync_crop
 from handleFusion import *
+from topLevel import quickplot
 import pickle
-import plot_dists as proto_plot
 
 def get_dir(datatype='',stage=''):
     homepath=os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -34,6 +34,8 @@ def get_dir(datatype='',stage=''):
         title='directory for cropped raw '+datatype+' time series data'
     elif stage=='proc':
         title='directory for processed '+datatype+' time series data'
+    else:
+        title='directory for '+datatype+' data' 
     set_dir=askdirectory(title=title,initialdir=homepath)
     return set_dir
 
@@ -43,13 +45,23 @@ class Rawfile:
         self.ppt=ppt
         self.label=label
         self.count=count
+        
+    def __eq__(self, other): #per https://stackoverflow.com/questions/1227121/compare-object-instances-for-equality-by-their-attributes
+        if not isinstance(other, Rawfile):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+
+        return self.filepath == other.filepath and \
+                self.ppt == other.ppt and \
+                    self.label == other.label and \
+                        self.count == other.count
     
     def __repr__(self):
         return(f'{self.__class__.__name__}('
-               f'{self.ppt!r}, {self.label!r}, {self.count!r})')
+               f'{self.filepath!r},{self.ppt!r}, {self.label!r}, {self.count!r})')
 
 def list_raw_files(raw_dir):
-    raw_list = None
+    raw_list = []
     for x in os.listdir(raw_dir):
         if not x.lower().endswith('.csv'): # Ignore non-CSV files
             continue    
@@ -68,7 +80,7 @@ def list_raw_files(raw_dir):
         raw_list.append(rawfile)
     return raw_list
 
-def sync_raw_files(raw_emg_list,raw_eeg_list,crop_emg_dir,crop_eeg_dir):
+def sync_raw_files(raw_emg_list,raw_eeg_list,crop_emg_dir,crop_eeg_dir,approval_required=0):
     for emgfile in raw_emg_list:
         print('working with '+repr(emgfile))
         raw_emg = matrix_from_csv_file(emgfile.filepath)
@@ -81,12 +93,13 @@ def sync_raw_files(raw_emg_list,raw_eeg_list,crop_emg_dir,crop_eeg_dir):
             continue
         print('found eeg: '+repr(eegfile))
         raw_eeg = matrix_from_csv_file(eegfile.filepath)
-        if 1:
+        raw_eeg=move_unicorn_time(raw_eeg)
+        if approval_required:
             crop_emg,crop_eeg=approve_sync(raw_emg, raw_eeg)
         else:
             crop_emg,crop_eeg=sync_crop(raw_emg, raw_eeg)
         np.savetxt(build_path(crop_emg_dir,emgfile),crop_emg,delimiter=',')
-        np.savetxt(build_path(crop_emg_dir,eegfile),crop_eeg,delimiter=',')
+        np.savetxt(build_path(crop_eeg_dir,eegfile),crop_eeg,delimiter=',')
         
 def build_path(dirpath,file):
     filepath=dirpath+'/'+file.ppt+'-'+file.label+'-'+file.count+'.csv'
@@ -110,6 +123,27 @@ def matrix_from_csv_file(file_path):
     full_matrix = csv_data[1:]
     #headers = csv_data[0] # Commented since not used or returned [fcampelo]
     return full_matrix
+
+def move_unicorn_time(unicorndata):
+    moved=unicorndata[:,[17,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18]]
+    return moved
+
+def do_something_brainflow(eeg):
+    print ("\nITS NOT FINISHED YET\n")
+    return eeg
+
+def ditch_bad_colums(eeg):
+    print ("\nITS NOT FINISHED YET\n")
+    return eeg
+
+def process_eeg(dataINdir,dataOUTdir):
+    eeglist=list_raw_files(dataINdir)
+    for eegfile in eeglist:
+        print('processing '+repr(emgfile))
+        eeg=matrix_from_csv_file(eegfile.filepath)
+        eeg=do_something_brainflow(eeg)
+        eeg=ditch_bad_columns(eeg)
+        np.savetxt(build_path(dataOUTdir,eegfile),eeg,delimiter=',')
     
 def whole_pipeline():
     raw_emg_dir=get_dir('emg','raw')
@@ -120,4 +154,5 @@ def whole_pipeline():
     proc_eeg_dir=get_dir('eeg','proc')
     raw_emg_list=list_raw_files(raw_emg_dir)
     raw_eeg_list=list_raw_files(raw_eeg_dir)
-    sync_raw_files(raw_emg_list,raw_eeg_list,crop_emg_dir,crop_eeg_dir)
+    sync_raw_files(raw_emg_list,raw_eeg_list,crop_emg_dir,crop_eeg_dir,approval_required=0)
+    process_eeg(crop_eeg_dir,proc_eeg_dir)

929031bf894ce95d01e1113dba5333cbb81a8215
Michael <mgpritchard123@yahoo.co.uk>
2022-12-12 13:25:40 +0000

    First pass at T&F subplot and adding trial name to plots
    (Not working due to erroneous removal of PSD plot in check_PSD, a change which was meant to be in the following commit)

--- verifUnicornData.py
+++ verifUnicornData.py
@@ -26,10 +26,6 @@ from brainflow.data_filter import DataFilter, FilterTypes, AggOperations, NoiseT
 from brainflow.exit_codes import BrainflowExitCodes #,BrainFlowError
 from brainflow.utils import check_memory_layout_row_major
 
-test_datafile='/home/michael/Documents/Aston/MultimodalFW/working_dataset/devset_EEG/Raw/0001a-grasp-2-_EEG.csv'
-trialname=test_datafile.split('/')[-1][:-9]
-print(trialname)
-
 def plot_signals(data,channels_to_plot,title='time domain',transposed=False):
     #df = pd.DataFrame(np.transpose(data))
     if transposed:
@@ -39,6 +35,7 @@ def plot_signals(data,channels_to_plot,title='time domain',transposed=False):
     #plt.axis([0,18,-100,1000])
     #plt.ylim([188000, 190000])
     df[channels_to_plot].plot(subplots=True)
+    '''plot against time, how to do with df?'''
     plt.title('time series signal '+title)
     plt.show()
     
@@ -47,15 +44,34 @@ def plot_psd(psd,title):
     plt.plot(psd[1],psd[0])
     plt.title('power spectral density '+title)
     plt.show()
+    
+def plot_t_and_f(data,channels_to_plot,psd,title,transposed=False):
+    if transposed:
+        data=data.transpose()
+    df = pd.DataFrame(data)
+    gs_kw=dict(width_ratios=[2,1])
+    fig,(ax1,ax2)=plt.subplots(1,2,gridspec_kw=gs_kw)
+    '''the following may work in matplotlib 3.6'''
+    #gs_kw = dict(width_ratios=[1.5, 1], height_ratios=[1, 2])
+    #fig,(ax1,ax2)=plt.subplot_mosaic([['Time','Freq']],gridspec_kw=gs_kw)
+    fig.suptitle(title)
+    df[channels_to_plot].plot(ax=ax1)
+    ax1.set_title('time series signal')
+    ax2.plot(psd[1],psd[0]) 
+    ax2.set_title('power spectral density')
+
 
 def check_PSD(data,channel,nfft,sampling_rate,title):
     psd=DataFilter.get_psd_welch(data[eeg_channel], nfft, nfft // 2, sampling_rate,
                                WindowFunctions.BLACKMAN_HARRIS.value)
-    plot_psd(psd,title)
     band_power_alpha = DataFilter.get_band_power(psd, 7.0, 13.0)
     band_power_beta = DataFilter.get_band_power(psd, 14.0, 30.0)
-    print("alpha/beta:%f", band_power_alpha / band_power_beta)
+    print("alpha/beta:%f" % (band_power_alpha / band_power_beta))
     return psd
+
+test_datafile='/home/michael/Documents/Aston/MultimodalFW/working_dataset/devset_EEG/Raw/0001a-grasp-2-_EEG.csv'
+trialname=test_datafile.split('/')[-1][:-9]
+print(trialname)
 
 board_id = BoardIds.UNICORN_BOARD.value
 params = BrainFlowInputParams()
@@ -68,7 +84,7 @@ nfft = DataFilter.get_nearest_power_of_two(sampling_rate)
 #eeg_channels = board_descr['eeg_channels']
 eeg_channels = [channel for channel in BoardShim.get_eeg_channels(board_id)]
 timestamp_channel = BoardShim.get_timestamp_channel(board_id)
-eeg_channel=eeg_channels[1]
+eeg_channel=eeg_channels[5]
 
 
 data,_=handleBF.load_raw_brainflow(datafile=test_datafile)
@@ -79,10 +95,10 @@ except Exception as e:
         data=data.transpose()
         check_memory_layout_row_major(data[eeg_channel],1)
         
-data=data[:,250:] #'''WHY IS THIS NEEDED, WHAT CAUSES IMPULSE. HARDWARE?'''
+data=data[:,500:] #'''WHY IS THIS NEEDED, WHAT CAUSES IMPULSE. HARDWARE?'''
 
-plot_signals(data,eeg_channel,title='before anything',transposed=True)
-check_PSD(data,eeg_channel,nfft,sampling_rate,title='before anything')
+plot_signals(data,eeg_channel,title=(trialname + ' before anything'),transposed=True)
+check_PSD(data,eeg_channel,nfft,sampling_rate,title=(trialname + ' before anything'))
 
 
 '''DETREND THE DATA'''
@@ -90,32 +106,40 @@ check_PSD(data,eeg_channel,nfft,sampling_rate,title='before anything')
 #https://dsp.stackexchange.com/questions/25189/detrending-in-real-time
 DataFilter.detrend(data[eeg_channel], DetrendOperations.CONSTANT.value)
 
-plot_signals(data,eeg_channel,title='after detrend',transposed=True)
-check_PSD(data,eeg_channel,nfft,sampling_rate,title='after detrend')
+plot_signals(data,eeg_channel,title=(trialname + ' after detrend'),transposed=True)
+check_PSD(data,eeg_channel,nfft,sampling_rate,title=(trialname + ' after detrend'))
 
+'''
+#TRYING TO REMOVE IMPULSE
+DataFilter.perform_highpass(data[eeg_channel], sampling_rate, 4.0, 6,
+                                        FilterTypes.BESSEL.value, 0) #0 is chebyshev ripple
+
+plot_signals(data,eeg_channel,title='after impulse killer',transposed=True)
+check_PSD(data,eeg_channel,nfft,sampling_rate,title='after impulse killer')
+'''
 
 '''50 Hz MAINS NOTCH'''
 DataFilter.perform_bandstop(data[eeg_channel], sampling_rate, 48.0, 52.0, 3,
                                         FilterTypes.BUTTERWORTH.value, 0)
 
-plot_signals(data,eeg_channel,title='after mains notch',transposed=True)
-check_PSD(data,eeg_channel,nfft,sampling_rate,title='after mains notch')
+plot_signals(data,eeg_channel,title=(trialname + ' after mains notch'),transposed=True)
+check_PSD(data,eeg_channel,nfft,sampling_rate,title=(trialname + ' after mains notch'))
 
 
 '''HIGH PASS'''
 DataFilter.perform_highpass(data[eeg_channel], sampling_rate, 3.0, 2,
                                         FilterTypes.BUTTERWORTH.value, 0) #0 is chebyshev ripple
 
-plot_signals(data,eeg_channel,title='after 2.0Hz HPF',transposed=True)
-check_PSD(data,eeg_channel,nfft,sampling_rate,title='after 2.0Hz HPF')
+plot_signals(data,eeg_channel,title=(trialname + ' after 2.0Hz HPF'),transposed=True)
+check_PSD(data,eeg_channel,nfft,sampling_rate,title=(trialname + ' after 2.0Hz HPF'))
 
 
 '''LOW PASS'''
 DataFilter.perform_lowpass(data[eeg_channel], sampling_rate, 90.0, 2,
                                         FilterTypes.BUTTERWORTH.value, 0) #0 is chebyshev ripple
 
-plot_signals(data,eeg_channel,title='after 90.0Hz LPF',transposed=True)
-check_PSD(data,eeg_channel,nfft,sampling_rate,title='after 90.0Hz LPF')
+plot_signals(data,eeg_channel,title=(trialname + ' after 90.0Hz LPF'),transposed=True)
+check_PSD(data,eeg_channel,nfft,sampling_rate,title=(trialname + ' after 90.0Hz LPF'))
 
 
 

7b83c715db6867ac396bfc737e28ac482a3f4bf7
Michael <mgpritchard123@yahoo.co.uk>
2022-12-21 00:09:20 +0000

    Moving process to __main__

--- verifUnicornData.py
+++ verifUnicornData.py
@@ -96,6 +96,7 @@ def plot_t_and_f(data,channels_to_plot,psd,title,transposed=False,PSD_xlim=None)
     #fig,(ax1,ax2)=plt.subplot_mosaic([['Time','Freq']],gridspec_kw=gs_kw)
     fig.suptitle(title)
     df[columns[channels_to_plot]].plot(ax=ax1)
+    #df.plot(x='Timestamp',y=[columns[channels_to_plot]],ax=ax1,y=columns[channels_to_plot])
     ax1.set_title('time series signal')
     ax2.plot(psd[1],psd[0]) 
     ax2.set_title('PSD')
@@ -112,143 +113,147 @@ def check_PSD(data,channel,nfft,sampling_rate):
     return psd
 
 
+if __name__ == '__main__':
 
-
-board_id = BoardIds.UNICORN_BOARD.value
-params = BrainFlowInputParams()
-board=BoardShim(board_id,params)
-#print(board.get_version()) #current ver is 4.2, get_version added in 5
-#board_descr = BoardShim.get_board_descr(board_id)
-#sampling_rate = int(board_descr['sampling_rate'])
-sampling_rate = BoardShim.get_sampling_rate(board_id)
-nfft = DataFilter.get_nearest_power_of_two(sampling_rate)
-#eeg_channels = board_descr['eeg_channels']
-eeg_channels = [channel for channel in BoardShim.get_eeg_channels(board_id)]
-timestamp_channel = BoardShim.get_timestamp_channel(board_id)
-eeg_channel=eeg_channels[5]
-
-
-
-
-#test_datafile='/home/michael/Documents/Aston/MultimodalFW/working_dataset/devset_EEG/Raw/0001a-grasp-2-_EEG.csv'
-test_datafile='/home/michael/Documents/Aston/MultimodalFW/working_dataset/devset_EEG/Cropped/0001a-grasp-2.csv'
-trialname=test_datafile.split('/')[-1]
-if trialname.endswith('EEG'):
-    trialname=trialname[:-9]
-else:
-    trialname=trialname[:-4]
-print(trialname)
-
-matched_emgfile='/home/michael/Documents/Aston/MultimodalFW/working_dataset/devset_EMG/Cropped/'+trialname+'.csv'
-emgdat=handleBF.matrix_from_csv_file(matched_emgfile)
-#data,_=handleBF.load_raw_brainflow(datafile=test_datafile)
-data,_=handleBF.load_raw_brainflow(datafile=test_datafile,bf_time_moved=True)
-if not data.shape[0]==len(eeg_channels)+1:
-    data=data.transpose()
-try:
-    check_memory_layout_row_major(data[eeg_channel],1)
-except Exception as e:
-    if e.exit_code==BrainflowExitCodes.INVALID_ARGUMENTS_ERROR.value:
-        data=data.copy(order='c')
-        #https://stackoverflow.com/questions/35800242/numpy-c-api-change-ordering-from-column-to-row-major
+    '''Setup Unicorn branflow etc'''
+    board_id = BoardIds.UNICORN_BOARD.value
+    params = BrainFlowInputParams()
+    board=BoardShim(board_id,params)
+    #print(board.get_version()) #current ver is 4.2, get_version added in 5
+    #board_descr = BoardShim.get_board_descr(board_id)
+    #sampling_rate = int(board_descr['sampling_rate'])
+    sampling_rate = BoardShim.get_sampling_rate(board_id)
+    nfft = DataFilter.get_nearest_power_of_two(sampling_rate)
+    #eeg_channels = board_descr['eeg_channels']
+    eeg_channels = [channel for channel in BoardShim.get_eeg_channels(board_id)]
+    timestamp_channel = BoardShim.get_timestamp_channel(board_id)
+    
+    '''pick channel to process'''
+    eeg_channel=eeg_channels[5]
+    
+    
+    
+    '''select datafile to process'''
+    #test_datafile='/home/michael/Documents/Aston/MultimodalFW/working_dataset/devset_EEG/Raw/0001a-grasp-2-_EEG.csv'
+    test_datafile='/home/michael/Documents/Aston/MultimodalFW/working_dataset/devset_EEG/Cropped/0001a-close-2.csv'
+    trialname=test_datafile.split('/')[-1]
+    if trialname.endswith('EEG'):
+        trialname=trialname[:-9]
+    else:
+        trialname=trialname[:-4]
+    print(trialname)
+    
+    '''load matching EMG data'''
+    matched_emgfile='/home/michael/Documents/Aston/MultimodalFW/working_dataset/devset_EMG/Cropped/'+trialname+'.csv'
+    emgdat=handleBF.matrix_from_csv_file(matched_emgfile)
+    
+    '''load EEG data and ensure its the right way round'''
+    #data,_=handleBF.load_raw_brainflow(datafile=test_datafile)
+    data,_=handleBF.load_raw_brainflow(datafile=test_datafile,bf_time_moved=True)
+    if not data.shape[0]==len(eeg_channels)+1:
+        data=data.transpose()
+    try:
         check_memory_layout_row_major(data[eeg_channel],1)
-
-#data=data[:,250:] #'''WHAT CAUSES IMPULSE. HARDWARE?'''
-
-
-psd=check_PSD(data,eeg_channel,nfft,sampling_rate)
-
-plot_t_and_f(data,eeg_channel,psd,'Before anything',transposed=True)
-#plot_signals(data,eeg_channel,title=(trialname + ' before anything'),transposed=True)
-#plot_psd(psd,title=(trialname + ' before anything'))
-
-
-'''DETREND THE DATA'''
-#DC blocking filter may be able to realtime detrend
-#https://dsp.stackexchange.com/questions/25189/detrending-in-real-time
-DataFilter.detrend(data[eeg_channel], DetrendOperations.CONSTANT.value)
-psd=check_PSD(data,eeg_channel,nfft,sampling_rate)
-
-plot_t_and_f(data,eeg_channel,psd,'Detrended',transposed=True)
-#plot_signals(data,eeg_channel,title=(trialname + ' after detrend'),transposed=True)
-#plot_psd(psd,title=(trialname + ' after detrend'))
-
-
-'''
-#TRYING TO REMOVE IMPULSE
-DataFilter.perform_highpass(data[eeg_channel], sampling_rate, 4.0, 6,
-                                        FilterTypes.BESSEL.value, 0) #0 is chebyshev ripple
-
-plot_signals(data,eeg_channel,title='after impulse killer',transposed=True)
-check_PSD(data,eeg_channel,nfft,sampling_rate,title='after impulse killer')
-'''
-
-
-'''50 Hz MAINS NOTCH'''
-DataFilter.perform_bandstop(data[eeg_channel], sampling_rate, 48.0, 52.0, 3,
-                                        FilterTypes.BUTTERWORTH.value, 0) #0 is chebyshev ripple
-psd=check_PSD(data,eeg_channel,nfft,sampling_rate)
-
-plot_t_and_f(data,eeg_channel,psd,'50Hz Mains Notch (3rd order Bwth)',transposed=True)
-#plot_signals(data,eeg_channel,title=(trialname + ' after mains notch'),transposed=True)
-#plot_psd(psd,title=(trialname + ' after 50Hz mains notch'))
-
-
-'''100 Hz MAINS HARMONIC'''
-DataFilter.perform_bandstop(data[eeg_channel], sampling_rate, 98.0, 102.0, 3,
-                                        FilterTypes.BUTTERWORTH.value, 0)
-psd=check_PSD(data,eeg_channel,nfft,sampling_rate)
-
-plot_t_and_f(data,eeg_channel,psd,'100Hz Mains Harmonic (3rd order Bwth)',transposed=True)
-
-
-
-
-'''HIGH PASS'''
-DataFilter.perform_highpass(data[eeg_channel], sampling_rate, 4.0, 2,
-                                        FilterTypes.BUTTERWORTH.value, 0) #maybe 5Hz??
-psd=check_PSD(data,eeg_channel,nfft,sampling_rate)
-
-plot_t_and_f(data,eeg_channel,psd,'2.0Hz HPF (2nd order Bwth)',transposed=True,PSD_xlim=[0, 30])
-#plot_signals(data,eeg_channel,title=(trialname + ' after 2.0Hz HPF'),transposed=True)
-#plot_psd(psd,title=(trialname + ' after 2.0Hz HPF'))
-
-
-'''LOW PASS'''
-'''
-DataFilter.perform_lowpass(data[eeg_channel], sampling_rate, 90.0, 2,
-                                        FilterTypes.BUTTERWORTH.value, 0)
-psd=check_PSD(data,eeg_channel,nfft,sampling_rate)
-
-plot_t_and_f(data,eeg_channel,psd,'90.0Hz LPF (2nd order Bwth)',transposed=True)
-#plot_signals(data,eeg_channel,title=(trialname + ' after 90.0Hz LPF'),transposed=True)
-#plot_psd(psd,title=(trialname + ' after 90.0Hz LPF'))
-'''
-
-
-
-plot_signals(emgdat,1,title='of the matching emg')
-
-'''print('EMG starting T: '+str(emgdat[0,0]))
-print('EMG ending T: '+str(emgdat[-1,0]))
-print('EEG starting T: '+str(data[0,0]))
-print('EEG ending T: '+str(data[0,-1]))'''
-
-'''referencing the timestamps to T0'''
-emgdat[:,0]=emgdat[:,0]-emgdat[0,0]
-data[0,:]=data[0,:]-data[0,0]
-'''print('EMG starting T: '+str(emgdat[0,0]))
-print('EMG ending T: '+str(emgdat[-1,0]))
-print('EEG starting T: '+str(data[0,0]))
-print('EEG ending T: '+str(data[0,-1]))'''
-
-n=nfft
-while n <= (len(data[eeg_channel])-len(data[eeg_channel])%nfft):
-    psd = check_PSD(data[:,n-nfft:n],eeg_channel,nfft,sampling_rate)
-    band_power_alpha = DataFilter.get_band_power(psd, 7.0, 13.0)
-    band_power_beta = DataFilter.get_band_power(psd, 14.0, 30.0)
-    print('Slice of '+trialname+' from '+str(n-nfft)+' to '+str(n)+': Alpha '+str(round(band_power_alpha,3))+', Beta '+str(round(band_power_beta,3)))
-    plot_t_and_f(data[:,n-nfft:n],eeg_channel,psd,('Slice from '+str(n-nfft)+' to '+str(n)),transposed=True,PSD_xlim=[0,30])
-    n+=(int(nfft/2))
-
-plot_eeg_and_emg(data,emgdat,5,4,'EMG and EEG for '+trialname)
+    except Exception as e:
+        if e.exit_code==BrainflowExitCodes.INVALID_ARGUMENTS_ERROR.value:
+            data=data.copy(order='c')
+            #https://stackoverflow.com/questions/35800242/numpy-c-api-change-ordering-from-column-to-row-major
+            check_memory_layout_row_major(data[eeg_channel],1)
+    
+    '''chop off start of recording, if not using cropped EEG'''
+    #data=data[:,250:] #'''WHAT CAUSES IMPULSE. HARDWARE?'''
+    
+    
+    '''compute PSD and plot sig & PSD before any processing'''
+    psd=check_PSD(data,eeg_channel,nfft,sampling_rate)
+    
+    plot_t_and_f(data,eeg_channel,psd,'Before anything',transposed=True)
+    #plot_signals(data,eeg_channel,title=(trialname + ' before anything'),transposed=True)
+    #plot_psd(psd,title=(trialname + ' before anything'))
+    
+    
+    '''DETREND THE DATA'''
+    #DC blocking filter may be able to realtime detrend
+    #https://dsp.stackexchange.com/questions/25189/detrending-in-real-time
+    DataFilter.detrend(data[eeg_channel], DetrendOperations.CONSTANT.value)
+    psd=check_PSD(data,eeg_channel,nfft,sampling_rate)
+    
+    plot_t_and_f(data,eeg_channel,psd,'Detrended',transposed=True)
+    
+    
+    '''
+    #TRYING TO REMOVE IMPULSE
+    DataFilter.perform_highpass(data[eeg_channel], sampling_rate, 4.0, 6,
+                                            FilterTypes.BESSEL.value, 0) #0 is chebyshev ripple
+    
+    plot_signals(data,eeg_channel,title='after impulse killer',transposed=True)
+    check_PSD(data,eeg_channel,nfft,sampling_rate,title='after impulse killer')
+    '''
+    
+    
+    '''50 Hz MAINS NOTCH'''
+    DataFilter.perform_bandstop(data[eeg_channel], sampling_rate, 48.0, 52.0, 3,
+                                            FilterTypes.BUTTERWORTH.value, 0) #0 is chebyshev ripple
+    psd=check_PSD(data,eeg_channel,nfft,sampling_rate)
+    
+    plot_t_and_f(data,eeg_channel,psd,'50Hz Mains Notch (3rd order Bwth)',transposed=True)
+    
+    
+    '''100 Hz MAINS HARMONIC'''
+    DataFilter.perform_bandstop(data[eeg_channel], sampling_rate, 98.0, 102.0, 3,
+                                            FilterTypes.BUTTERWORTH.value, 0)
+    psd=check_PSD(data,eeg_channel,nfft,sampling_rate)
+    
+    plot_t_and_f(data,eeg_channel,psd,'100Hz Mains Harmonic (3rd order Bwth)',transposed=True)
+       
+    
+    '''HIGH PASS'''
+    DataFilter.perform_highpass(data[eeg_channel], sampling_rate, 4.0, 2,
+                                            FilterTypes.BUTTERWORTH.value, 0) #maybe 5Hz??
+    psd=check_PSD(data,eeg_channel,nfft,sampling_rate)
+    
+    plot_t_and_f(data,eeg_channel,psd,'2.0Hz HPF (2nd order Bwth)',transposed=True,PSD_xlim=[0, 30])
+    
+    
+    '''LOW PASS'''
+    #deprecated in favour of notch filtering 100Hz harmonic
+    '''
+    DataFilter.perform_lowpass(data[eeg_channel], sampling_rate, 90.0, 2,
+                                            FilterTypes.BUTTERWORTH.value, 0)
+    psd=check_PSD(data,eeg_channel,nfft,sampling_rate)
+    
+    plot_t_and_f(data,eeg_channel,psd,'90.0Hz LPF (2nd order Bwth)',transposed=True)
+    #plot_signals(data,eeg_channel,title=(trialname + ' after 90.0Hz LPF'),transposed=True)
+    #plot_psd(psd,title=(trialname + ' after 90.0Hz LPF'))
+    '''
+    
+    
+    
+    plot_signals(emgdat,1,title='of the matching emg')
+    
+    '''print('EMG starting T: '+str(emgdat[0,0]))
+    print('EMG ending T: '+str(emgdat[-1,0]))
+    print('EEG starting T: '+str(data[0,0]))
+    print('EEG ending T: '+str(data[0,-1]))'''
+    
+    '''referencing the timestamps to T0'''
+    emgdat[:,0]=emgdat[:,0]-emgdat[0,0]
+    data[0,:]=data[0,:]-data[0,0]
+    '''print('EMG starting T: '+str(emgdat[0,0]))
+    print('EMG ending T: '+str(emgdat[-1,0]))
+    print('EEG starting T: '+str(data[0,0]))
+    print('EEG ending T: '+str(data[0,-1]))'''
+    
+    '''sliding window across data to get PSD plots through time'''
+    n=nfft
+    while n <= (len(data[eeg_channel])-len(data[eeg_channel])%nfft):
+        psd = check_PSD(data[:,n-nfft:n],eeg_channel,nfft,sampling_rate)
+        band_power_alpha = DataFilter.get_band_power(psd, 7.0, 13.0)
+        band_power_beta = DataFilter.get_band_power(psd, 14.0, 30.0)
+        print('Slice of '+trialname+' from '+str(n-nfft)+' to '+str(n)+': Alpha '+str(round(band_power_alpha,3))+', Beta '+str(round(band_power_beta,3)))
+        plot_t_and_f(data[:,n-nfft:n],eeg_channel,psd,('Slice from '+str(n-nfft)+' to '+str(n)),transposed=True,PSD_xlim=[0,30])
+        n+=(int(nfft/2))
+    #df['Timestamp'].plot(xlim=([850,900]),ylim=([3400,3600]))
+    
+    '''plotting both modalities together'''
+    plot_eeg_and_emg(data,emgdat,5,4,'EMG and EEG for '+trialname)

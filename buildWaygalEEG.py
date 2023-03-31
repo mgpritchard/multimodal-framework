460c72d314550807ad9c108f030be965466d94b1
Michael <mgpritchard123@yahoo.co.uk>
2023-03-31 21:02:06 +0100

    making script less dataset-specific

--- buildWaygalEEG.py
+++ buildWaygalEEG.py
@@ -8,12 +8,22 @@ Created on Wed Mar 29 20:45:43 2023
 import params as params
 import handleFeats as feats
 
+dataset='flarp'
+
 skipfails=True
 period=1
 datatype='eeg'
 
-directory_path=params.all_channel_waygal_EEG
-output_file=params.eeg_32_waygal
+if dataset=='waygal':
+
+    directory_path=params.all_channel_waygal_EEG
+    output_file=params.eeg_32_waygal
+    
+elif dataset=='jeong':
+    directory_path=params.jeongEEGdir
+    output_file=params.eeg_jeong_feats
+else:
+     raise ValueError('I don\'t know what dataset you mean by '+dataset)   
 
 print('Are the following parameters OK?')
 print('Skipfails: ',skipfails,'\n',

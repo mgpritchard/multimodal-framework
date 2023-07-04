9ebbd23eefce716645de0a900cc8264c067a60b7
Michael <mgpritchard123@yahoo.co.uk>
2023-07-04 15:49:54 +0100

    Bash scripts for testing all architectures in generalist server case

---
+++ bash_all_archs_generalist_Once.bash
@@ -0,0 +1,26 @@
+#!/bin/bash 
+cd /users/p/pritcham/multimodal-opt/mm-opt-experiments/
+
+python testFusion.py decision server 1 False
+zip lit_data_expts/jeong/results/decision.zip lit_data_expts/jeong/results/Fusion_CSP/LOO/*
+rm lit_data_expts/jeong/results/Fusion_CSP/LOO/*
+
+python testFusion.py hierarchical LOO server 1 False
+zip lit_data_expts/jeong/results/hierarchical_prob.zip lit_data_expts/jeong/results/Fusion_CSP/LOO/*
+rm lit_data_expts/jeong/results/Fusion_CSP/LOO/*
+
+python testFusion.py hierarchical_inv LOO server 1 False
+zip lit_data_expts/jeong/results/hierarchical_inv_prob.zip lit_data_expts/jeong/results/Fusion_CSP/LOO/*
+rm lit_data_expts/jeong/results/Fusion_CSP/LOO/*
+
+python testFusion.py just_emg server 1 False
+zip lit_data_expts/jeong/results/EMG_only.zip lit_data_expts/jeong/results/Fusion_CSP/LOO/*
+rm lit_data_expts/jeong/results/Fusion_CSP/LOO/*
+
+python testFusion.py just_eeg server 1 False
+zip lit_data_expts/jeong/results/EEG_only.zip lit_data_expts/jeong/results/Fusion_CSP/LOO/*
+rm lit_data_expts/jeong/results/Fusion_CSP/LOO/*
+
+python testFusion.py featlevel server 1 False
+zip lit_data_expts/jeong/results/featlevel.zip lit_data_expts/jeong/results/Fusion_CSP/LOO/*
+rm lit_data_expts/jeong/results/Fusion_CSP/LOO/*
